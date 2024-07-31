79e49a1c2828ba5a828780bfee76d8f7e62d0e17
apollostudios159 <hardit826@gmail.com>
2024-07-31 23:00:21 +0530

    Added Primary annotation implementation

---
+++ 05-primary/src/main/java/com/springcorelearn/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,12 @@
+package com.springcorelearn.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+
+@Component //Marks the class as SpringBean and makes it available for dependency injection
+public class CricketCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes";
+    }
+}
