891b8c2e19410a8e31e0efd7b002081028c84aac
apollostudios159 <hardit826@gmail.com>
2024-07-31 20:53:52 +0530

    added setter injections and component scan

---
+++ 03-setter-injection/src/main/java/com/springcorelearn/springcoredemo/common/Coach.java
@@ -0,0 +1,5 @@
+package com.springcorelearn.springcoredemo.common;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ 03-setter-injection/src/main/java/com/springcorelearn/springcoredemo/rest/DemoController.java
@@ -0,0 +1,24 @@
+package com.springcorelearn.springcoredemo.rest;
+
+import com.springcorelearn.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    //Define private field for the dependency
+    private Coach myCoach;
+
+    //Define the setter method
+    @Autowired
+    public void setCoach(Coach theCoach){
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
